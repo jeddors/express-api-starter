d312f7e6130d42f1a1e2f870fced51e2bb79fd6b
jeddors <70684449+jeddors@users.noreply.github.com>
2021-01-14 00:14:48 +0200

    auth middleware for protected endpoints / MongoDB object ID middleware

--- src/api/users.js
+++ src/api/users.js
@@ -1,12 +1,38 @@
 const express = require('express');
+const auth = require('../middlewares/auth');
+const checkObjectId = require('../middlewares/checkObjectId');
 
 const router = express.Router();
 
+const User = require('../models/User');
+
+// @route    GET api/users
+// @desc     route status
+// @access   Public
 router.get('/', (req, res) => {
   res.json({
       message: "you've reached the base URL for the /users route!",
       timestamp: new Date().getUTCMilliseconds();
   });
 });
+
+// @route    GET api/posts/:id
+// @desc     Get post by ID
+// @access   Private
+// router.get('/:id', auth, checkObjectId('id'), async (req, res) => {
+//     try {
+//       const post = await Post.findById(req.params.id);
+  
+//       if (!post) {
+//         return res.status(404).json({ msg: 'Post not found' });
+//       }
+  
+//       res.json(post);
+//     } catch (err) {
+//       console.error(err.message);
+  
+//       res.status(500).send('Server Error');
+//     }
+//   });
 
 module.exports = router;
